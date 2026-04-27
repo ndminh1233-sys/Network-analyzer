c2d284ee011ad14ba07fcb0cc6ceb83bf17c7dd4
NguyenMinh <ndminh1233@gmail.com>
2026-04-27 21:18:30 +0300

    Added anomaly detection

--- packet_sniff/main.py
+++ packet_sniff/main.py
@@ -8,7 +8,10 @@ tcp_count = 0
 udp_count = 0
 icmp_count = 0
 total_count = 0
+last_seen_ip = None
 newfile_info = []
+ANOMALY_THRESHOLD = 10
+ip_count = {}
 
 def main():
     global newfile_info
@@ -24,7 +27,7 @@ def main():
     print("ICMP: " + str(icmp_count))
     print("Total: " + str(tcp_count + udp_count + icmp_count))
 
-    user = input('Are you sure? (y/n): ').lower().startswith('y')
+    user = input('Do you want to print the result into your file? ').lower().startswith('y')
 
     if(user == True):
         user_inter()
@@ -35,9 +38,10 @@ def packet_analysis(packet):
     result  = datetime.fromtimestamp(packet.time)
     global tcp_count, udp_count, icmp_count
     global newfile_info
-
+    anomaly_detection(packet)
     if(packet.haslayer(IP)):
         if(packet. haslayer(TCP)):
+            
             line = result.strftime("[%H:%M:%S] ") + "Packet " + str(tcp_count + 1) + " | " + "TCP " + packet[IP].src + ": " + str(packet[TCP].sport) + " -> " + packet[IP].dst + ": " + str(packet[TCP].dport)
             print(line)
             tcp_count+=1
@@ -63,6 +67,25 @@ def user_inter():
     newfile_info.append("ICMP: " + str(icmp_count))
     newfile_info.append("Total: " + str(tcp_count + udp_count + icmp_count))
     
+def anomaly_detection(packet):
+    global last_seen_ip
+    if not packet.haslayer(IP):
+        return
+
+    src_ip = packet[IP].src
+
+    if src_ip == last_seen_ip:
+        ip_count[src_ip] += 1 
+    else:
+        ip_count[src_ip] = 1
+        last_seen_ip = src_ip
+
+    if ip_count[src_ip] == ANOMALY_THRESHOLD:
+        alert = f"[⚠️  ALERT] {src_ip} appeared {ANOMALY_THRESHOLD} times consecutively — possible scan!"
+        print(alert)
+        newfile_info.append(alert)
+
+
 
 
 if __name__ == "__main__":
